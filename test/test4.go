4a4610c172ea52bf43cec6d044cabd73ec7337be
Lien Le <lienle.2026@icloud.com>
2025-09-16 11:46:26 +0700

    add file test4.go

---
+++ test/test4.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 1
+
+	switch i {
+	case 1:
+		fmt.Printf("case 1 case 1 case 1 case 1 case 1 case 1\n")
+		break
+	default:
+		fmt.Printf("default default default default default default \n")
+		break
+	}
+}
