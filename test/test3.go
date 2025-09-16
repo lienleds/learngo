af47f752ecb29677c41c3e2bb987542165d7a1d0
Lien Le <lienle.2026@icloud.com>
2025-09-16 11:42:12 +0700

    add file test3.go

---
+++ test/test3.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func f(t int) string {
+	ret := "vaule = " + fmt.Sprint(math.Sqrt(float64(t)))
+	return ret
+}
+
+func main() {
+	fmt.Printf("jldfksaskljfkldsj %v\n", f(-1))
+	fmt.Printf("fjkldasfkldsajfkldsajfkodsaj\n")
+}
